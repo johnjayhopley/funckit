a0584e7f648703c79ff68af139a1bbed2ae1fb81
James Coventry <jamescoventryy@gmail.com>
2018-03-22 10:14:47 +0000

    add intersection

---
+++ src/array/intersection.js
@@ -0,0 +1,27 @@
+/**
+ * @brief Array Intersection
+ * @author James Coventry <james.coventry@readingroom.com>
+ * @details Takes in multiple array types and returns
+ * all values contained in every array
+ *
+ * intersection([1,2,3,4],[2,3,4],[1,2,2,3,3])
+ * => [2,3]
+ *
+ * @param  {Array} arr
+ * @return {Array}
+ */
+
+import first from './first';
+
+export default (...arrays) => {
+  arrays.forEach((arr, i) => {
+    if (!Array.isArray(arr)) {
+      throw new Error(
+        `Intersection expects argument ${i + 1} to be of type Array.`,
+      );
+    }
+  });
+
+  return first(arrays).filter(val =>
+    arrays.slice(1).every(arr => arr.includes(val)));
+};
